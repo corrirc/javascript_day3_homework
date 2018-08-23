7cf981b87db459acec2cb90346f857a8134b8780
Ricky <rickycorrigan@icloud.com>
2018-08-23 10:02:40 +0100

    all tests passing

--- models/traveller.js
+++ models/traveller.js
@@ -38,11 +38,17 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  let transportation = this.getModesOfTransport();
+  return transportation.filter((mode, index) => {
+    return transportation.indexOf(mode) === index;
+  });
+  // return [... new Set(this.getModesOfTransport() )];
 };
 
 
